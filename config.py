aa80266f3d78d05ef7b72dd9503964cff490622d
beproalways <srcbhaisrc@gmail.com>
2026-09-12 08:11:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ DB_NAME      = os.getenv("DB_NAME", "srcbhaisrc")
 # ─── OWNER / CONTROL SETTINGS ───────────────────────────────────────────────────
 OWNER_ID     = list(map(int, os.getenv("OWNER_ID", " 7915380485").split()))  # space-separated list
 STRING       = os.getenv("STRING", None)  # optional session string
-LOG_GROUP    = int(os.getenv("LOG_GROUP", ""))
+LOG_GROUP    = int(os.getenv("LOG_GROUP", "10044682251662"))
 FORCE_SUB    = int(os.getenv("FORCE_SUB", ""))
 
 # ─── SECURITY KEYS ──────────────────────────────────────────────────────────────
